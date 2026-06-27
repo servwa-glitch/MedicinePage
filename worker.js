633685f49525799b92ede9d6fcb5da43727bd4aa
servwa-glitch <servwa@gmail.com>
2026-06-27 18:03:58 +0800

    Update worker0627.js

--- worker.js
+++ worker.js
@@ -36,7 +36,7 @@ export default {
           "agree": true,
           prompt: promptText,
           image: [...imgArray],
-          max_tokens: 2048          
+          max_tokens: 2048      //2048    
         });
 
         // 5. 將結果包裝成你前端需要的格式回傳 (data.content[0].text)
